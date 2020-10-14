ce8a907b03176e8a60c27a18cfbaa02ff1d17cc2
Deybison_Perez <dsperez784@misena.edu.co>
2020-10-13 19:11:54 -0500

    actualizacion_1

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns =[
-    path('',vista_about,name='about'),
+    path('',vista_registrar,name='about'),
+    path('lista',vista_lista,name= 'lista'),
+    path('registrar cuenta',vista_registrar_cuenta, name= 'registrar cuenta'),
+    path('cliente',vista_cliente,name='cliente'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-def vista_about(request):
-    return render(request,'about.html')
+def vista_registrar(request):
+    return render(request,'registrar.html')
+
+
+def vista_lista(request):
+    return render (request,'lista.html')
+
+def vista_registrar_cuenta(request):
+    return render(request, 'registrar_cuenta.html')
+
+def vista_cliente(request):
+    return render(request, 'cliente.html')
